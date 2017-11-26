410ed4c9c179f99018a04d5dc3942d55ffd8d40a
epakosu <pake@onid.oregonstate.edu>
2017-11-26 13:43:54 -0500

    Update file_handler.hpp

--- file_handler.hpp
+++ file_handler.hpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 
-std::vector<std::vector<int>> file_to_arr(std::string);
+std::vector<std::vector<int>> file_to_arr(std::string file);
 void arr_to_file(std::string, std::vector<int>);
 
 #endif /* file_handler_hpp */
