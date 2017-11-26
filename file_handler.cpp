fe4f05b4a46d0f6fd2ba733232febfd012e5bf12
epakosu <pake@onid.oregonstate.edu>
2017-11-26 13:43:33 -0500

    Update file_handler.cpp
    Added functions to compute euclidean distance and create adjacency matrix

--- file_handler.cpp
+++ file_handler.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include "file_handler.hpp"
+#include "tsp.hpp"
 #include <string>
 #include <vector>
 #include <iterator>
+#include <sstream>
+#include <math.h>
+
+/*
+   Params:
+      x and y coordinates for two cities
+   Returns:
+      euclidean distance between cities
+*/
+
+int compute_distance(int x1, int y1, int x2, int y2) {
+   double dx = x2 - x1;
+   double dy = y2 - y1;
+   double dist = sqrt(dx*dx + dy*dy);
+   return (int) round(dist);
+}
 
 /*
    Params:
@@ -13,17 +30,32 @@
 */
 std::vector<std::vector<int>> file_to_arr(std::string ifile) {
    std::string line;
-   std::ifstream file (ifile);
-   int one;
-   int two;
-   int three;
-   std::vector<std::vector<int>> arr;
+   std::ifstream file;
+   std::vector<City> cities;
+   std::vector<std::vector<int> > adj_matrix;
+   file.open(ifile.c_str());
 
-   while(file >> one >> two >> three) {
-      arr.push_back({one, two, three});
+   while(getline(file, line)) {
+      std::istringstream iss(line);
+      City input_city;
+      iss >> input_city.id;
+      iss >> input_city.x;
+      iss >> input_city.y;
+
+      cities.push_back(input_city);
    }
 
-   return arr;
+   file.close();
+   int num_cities = cities.size();
+   adj_matrix.resize(num_cities, std::vector<int>(num_cities, -1));
+
+   for(int i = 0; i < num_cities; i++) {
+      for(int j = 0; j < num_cities; j++) {
+         adj_matrix[i][j] = compute_distance(cities[i].x, cities[i].y, cities[j].x, cities[j].y);
+      }
+   }
+
+   return adj_matrix;
 }
 
 
@@ -39,7 +71,3 @@ void arr_to_file(std::string ofile, std::vector<int> arr) {
       file << arr[i] << "\n";
    }
 }
-
-
-
-
