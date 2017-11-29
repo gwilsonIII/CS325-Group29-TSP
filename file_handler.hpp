30771d1111f4568140546dd771dcdb24c3ad39b0
epakosu <pake@onid.oregonstate.edu>
2017-11-29 00:54:15 -0800

    Update file_handler.hpp

--- file_handler.hpp
+++ file_handler.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 std::vector<std::vector<int>> file_to_arr(std::string ifile);
-void arr_to_file(std::string, std::vector<int>);
+void arr_to_file(std::string out_file_name, std::vector<int> tour, int cost);
 
 struct City {
    int id, x, y;
