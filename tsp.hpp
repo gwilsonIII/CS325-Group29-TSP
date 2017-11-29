b06a17abce271d3f7c0774c3823c42f9a72d72ff
epakosu <pake@onid.oregonstate.edu>
2017-11-29 00:55:05 -0800

    Update tsp.hpp

--- tsp.hpp
+++ tsp.hpp
@@ -4,5 +4,7 @@
 #include <vector>
 
 std::vector<int> tsp(std::vector<std::vector<int>>);
+std::vector<int> nearest_neighbor(std::vector<std::vector<int> > adj_matrix);
+std::vector<int> two_opt(std::vector<std::vector<int> > adj_matrix, std::vector<int> tour);
 
 #endif /* tsp_hpp */
